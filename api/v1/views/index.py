77ca7c9bab25d9e4c3e81355facebf66dae4f337
SeckMohameth <mohamethseck34@gmail.com>
2019-09-11 18:58:31 +0000

    task 5 for counting objects using count()

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,3 +5,16 @@ from api.v1.views import app_views
 def status():
     '''status'''
     return jsonify({"status": "OK"})
+
+@app_views.route('/status')
+def status():
+    '''retrieves the number of each objects by type'''
+    stats = {
+        'amenities': storage.count('Amenity'),
+        'cities': storage.count('City'),
+        'places': storage.count('Place'),
+        'reviews': storage.count('Review'),
+        'states': storage.count('State'),
+        'users': storage.count('User')
+    }
+    return jsonify(stats)
