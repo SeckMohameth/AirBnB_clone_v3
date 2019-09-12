e0c9ab75ea8c1c6bff42a489116c65f585c0ec16
DracoMindz <43617819+DracoMindz@users.noreply.github.com>
2019-09-12 11:14:51 -0400

    State: includes views, need delete

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,6 +1,58 @@
 #!/usr/bin/python3
+"""State Class"""
+
 from models import storage
 from api.v1.views import app_views
-from flask import Flask, Blueprint, make_response, jsonify, request
+from flask import Flask, abort, make_response, jsonify, request
 from os import getenv
-from flask_cors import CORS
+from models.state import State
+
+@app_views.route('/states', methods=['GET'], strict_slashes=False)
+def get_state():
+    """retrieve state list (var = st_list) from json"""
+    st_list = [obj.to_dict() for obj in storage.all('State').values()]
+    return jsonify(st_list)
+
+@app_views.route('/states/<string:state_id>', methods=['GET'], strict_slashes=False)
+    def get_stateId(state_id):
+        """retrieve state object with id"""
+        state = storage.get('State', state_id)
+        if state is None:
+            abort(404)
+        return jsonify(state.to_dict())
+
+
+@app_views.route('/states/<string:state_id', methods=['DELETE'], strict_slashes=False)
+    def del_stateId(state_id):
+        """delete state object if given state id"""
+    state = 
+
+
+
+@app_views.route('/states', methods=['POST'], strict_slashes=False)
+    def create_state():
+        """create a dict from HTTP body request"""
+        """new state object"""
+        if not request.get_json():
+            return jsonify({"error": "Not a JSON"}), 400
+        if not 'name' in request.get_json():
+            return jsonify({"error": "Missing name"}), 400
+        object_data = request.get_json()
+        object = State(**object_data)
+        object.save()
+        return jsonify(object.to_dict()), 201
+
+
+@app_views.route('/states/<string:state_id', methods=['PUT'], strict_slashes=False)
+    def update_state(state_id):
+        """update state obj with key value pair"""
+        if not request.get_json():
+            return jsonify({"error": "Not a JSON"}), 400
+        object = storage.get('State', state_id)
+        if object is None:
+            abort(404)
+        object_data = request.get_json()
+        object.name = object_data['name']
+        object.save()
+        return jsonify(object.to_dict()), 200
+        
