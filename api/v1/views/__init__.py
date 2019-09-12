1d5e90ed70a19240555a458e9b28a2edf76da7dc
DracoMindz <43617819+DracoMindz@users.noreply.github.com>
2019-09-12 03:21:24 -0400

    Update:  imports from models and api.v1.views

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,7 +1,16 @@
 #!/usr/bin/python3
 from flask import Blueprint
-
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
+
+"""import models and storage"""
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.user import User
+from models.place import Place
+from models.review import Review
+
 
 if app_views is not None:
     from api.v1.views.index import *
