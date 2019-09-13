5efdde181f5f35168399c72ed0fcdcee534411af
DracoMindz <43617819+DracoMindz@users.noreply.github.com>
2019-09-13 07:19:35 -0400

    Update:changes to env

--- api/v1/app.py
+++ api/v1/app.py
@@ -29,5 +29,5 @@ def error_handler(error):
 
 
 if __name__ == "__main__":
-    app.run(host=getenv('HBNB_API_HOST', '0.0.0.0'),
-            port=int(getenv('HBNB_API_PORT', '5000')))
+    app.run(host=getenv("HBNB_API_HOST", "0.0.0.0"),
+            port=int(getenv("HBNB_API_PORT", "5000"))threaded=True)
